2e19e91095fccc72a4192b05f3fc665ef9af41d9
Jim Guccione <jim@sketchdev.io>
2018-09-18 14:26:26 -0500

    gnlb added from html not components

---
+++ components/Navigation.js
@@ -0,0 +1,36 @@
+    export default`
+    <div id="navigation">
+    
+    <ul class="container">
+        <li>
+            <a href="Year">Year</a>
+            <ul>
+                <li>Freshman</li>
+                <li>Sophomore</li>
+                <li>Junior</li>
+                <li>Senior</li>
+            </ul>
+        </li>
+    
+        <li>
+            <a href="Requirements">Requirements</a>
+            <ul>
+                <li>Training</li>
+                <li>Grades</li>
+                <li>Coaches/li>
+            </ul>
+            </li>
+    
+            <li>
+                <a href="Calendar">Calendar</a>
+                <ul>
+                    <li>Jan-Mar</li>
+                    <li>Apr-Jun</li>
+                    <li>Jul-Sep</li>
+                    <li>Oct-Dec</li>
+                </ul>
+            </li>
+    
+    </div>
+    `;
+    
--- index.js
+++ index.js
@@ -1,6 +1,170 @@
-import Navigation from './Components/Navigation';
-import Header from './Components/Header';
-import Content from './Components/Content';
-import Footer from './Components/Footer';
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
 var initialHTML = document.body.innerHTML;
+
+
+var State = {
+    'active': 'home',
+
+    'home': {
+        'title': 'Welcome to my Port'
+    },
+
+    'blog': {
+        'title': 'Welcome to Blog'
+    },
+
+    'projects': {
+        'title': 'Welcome to Projects'
+    },
+};
+// this keeps all the navigation
+
+document
+    .body
+    .innerHTML = `
+      ${Navigation}
+      ${Header}
+      ${Content}
+      ${Footer}
+      ${initialHTML} // we still need this
+    `;
+
+function handleNavigation(event){
+    render(newState);
+
+    var newState = state;
+
+    newState.active = event.target.textContent;
+
+    event.preventDefault();
+}
+// wtf look for State and newState
+
+
+
+
+// console.log(event.target.textContent);
+
+function render(state){
+    var i = 0;
+    var links;
+
+    // USE THIS!!
+     
+    document
+        .querySelector('#root')
+        .innerHTML = `
+        ${Navigation()}
+        ${Header(state)[state.active]}
+        ${Content()}
+        ${Footer()}
+        `;
+
+    links = document.querySelectorAll('#navigation a');
+
+    for(let  i = 0; i < links.length; i++){
+        links[i].addEventListener(
+            'click',
+            handleNavigation);
+    }
+
+    render(State);
+    // kick starts everything
+
+    document
+        .querySelector('#navigation')
+        .innerHTML = Navigation;
+
+    document
+        .querySelector('#header')
+        .innerHTML = Header;
+
+    document
+        .querySelector('#content')
+        .innerHTML = Content;
+    
+    document
+        .querySelector('#footer')
+        .innerHTML = Footer;
+
+    links[0].addEventListener(
+        'click',
+        handleNavigation
+    );
+
+    links[1].addEventListener(
+        'click',
+        handleNavigation
+    );
+    links[2].addEventListener(
+        'click',
+        handleNavigation
+    );
+
+
+
+document
+    .body
+    .innerHTML = `
+      ${Navigation}
+      ${Header(state)}
+      ${Content}
+      ${Footer}
+      ${initialHTML} // we still need this
+    `;
+
+    render(State);
+    // kick starts everything
+
+    document
+        .querySelector('#navigation')
+        .innerHTML = Navigation;
+
+    document
+        .querySelector('#header')
+        .innerHTML = Header;
+
+    document
+        .querySelector('#content')
+        .innerHTML = Content;
+    
+    document
+        .querySelector('#footer')
+        .innerHTML = Footer;
+
+    links[0].addEventListener(
+        'click',
+        handleNavigation
+    );
+
+
+
+
+
+    links[1].addEventListener(
+        'click',
+        handleNavigation
+    );
+    links[2].addEventListener(
+        'click',
+        handleNavigation
+    );
+}
+
+function render(state){
+   var links;
+
+   console.log('router->', router);
+
+
+    console.log('router->', router);
+
+    router
+    .on('/:page', (params) => handleNavigation(params.page))
+    .on('/', () => handleNavigation('home'))
+    .resolve();
+}
