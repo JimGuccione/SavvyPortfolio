336ae52acff7dd72b1785958a4911a26c5fd4025
Jim Guccione <jim@sketchdev.io>
2018-09-18 12:11:26 -0500

    getting ready to revert

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 __Bold__
 
 
-![Bryce Harper](https://rfathead-res.cloudinary.com/image/upload/c_scale,f_auto,h_600,q_auto,w_600/v1506448124/assets/web/51-51803_mlb_bryce_harper_2017_realbig_pdp_prod.png)
+![Coach Guccione](https://pbs.twimg.com/profile_images/810482897304043520/XjQc3PFn_400x400.jpg)
 
 
 
--- store/index.js
+++ store/index.js
@@ -1,6 +1,6 @@
-export { default as active } from './Active';
-export { default as blog } from './Blog';
-export { default as contact } from './Contact';
-export { default as home } from './Home';
-export { default as posts } from './Posts';
-export { default as projects } from './Projects';
+export { default as active } from './components/Active';
+export { default as blog } from './components/Blog';
+export { default as contact } from './components/Contact';
+export { default as home } from './components/Home';
+export { default as posts } from './components/Posts';
+export { default as projects } from './components/Projects';
